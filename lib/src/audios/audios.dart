ef4694f08979b9dbcb416f28f6af29f3557915c8
tuthanika <tuthanika@gmail.com>
2026-03-26 22:21:25 +0700

    đổi thư viện sound mặc định thành dạng url

--- lib/src/audios/audios.dart
+++ lib/src/audios/audios.dart
@@ -1,27 +1,200 @@
+// Thay "https://domain-cua-ban.com/audios/" bằng link host thực tế của bạn
+const String _baseUrl = "https://raw.githubusercontent.com/tuthanika/ATP-Sound-Audio-File/refs/heads/main/audios";
+
 List assetsAudios = const [
-{'path': "assets/audios/Bard's_Lute.mp3",'image': 'assets/images/tavern.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Bard_chill_by_Oliver_Getzp.mp3",'image': 'assets/images/tavern.jpg', 'audio_source': 'assets', 'version_number': 16},
-{'path': "assets/audios/Battlefield.mp3",'image': 'assets/images/battlefield.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Cavern.mp3",'image': 'assets/images/cavern.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Distant_Thunder.mp3",'image': 'assets/images/rain.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Drafty_Castle.mp3",'image': 'assets/images/demon.jpg', 'audio_source': 'assets', 'version_number': 16},
-{'path': "assets/audios/Fireplace.mp3",'image': 'assets/images/tavern.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Generic_Dungeon.mp3",'image': 'assets/images/cavern.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Gregorian_Voices.mp3",'image': 'assets/images/heaven.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Hearing_Voices.mp3",'image': 'assets/images/market.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Heavy_Rain.mp3",'image': 'assets/images/rain.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Into_Battle.mp3",'image': 'assets/images/battlefield.jpg', 'audio_source': 'assets', 'version_number': 16},
-{'path': "assets/audios/Morning_Birds.mp3",'image': 'assets/images/sea.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Night_Forest.mp3",'image': 'assets/images/forest.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Our_Mountain.mp3",'image': 'assets/images/deer.jpg', 'audio_source': 'assets', 'version_number': 16},
-{'path': "assets/audios/Pirates.mp3",'image': 'assets/images/sea.jpg', 'audio_source': 'assets', 'version_number': 16},
-{'path': "assets/audios/Rain.mp3",'image': 'assets/images/rain.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Sacred_Valley.mp3",'image': 'assets/images/heaven.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Sea_Coast.mp3",'image': 'assets/images/sea.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Tavern.mp3",'image': 'assets/images/tavern.jpg', 'audio_source': 'assets', 'version_number': 1},
-{'path': "assets/audios/Village.mp3",'image': 'assets/images/market.jpg', 'audio_source': 'assets', 'version_number': 1},
+  {
+    'name': "Bard's Lute",
+    'path': "$_baseUrl/Bard's_Lute.mp3",
+    'image': 'assets/images/tavern.jpg', 
+    'audio_source': 'file', // QUAN TRỌNG: Ép thành 'file' để kích hoạt logic Tải/Stream
+    'folder_name': 'Thư viện Cloud', // Gom chung vào 1 Folder
+    'is_offline_mode': true, // True: Tải về khi phát lần đầu. False: Stream trực tiếp
+    'version_number': 17 // Đổi version để ép app cập nhật
+  },
+  {
+    'name': "Bard chill",
+    'path': "$_baseUrl/Bard_chill_by_Oliver_Getzp.mp3",
+    'image': 'assets/images/tavern.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Battlefield",
+    'path': "$_baseUrl/1/Battlefield.mp3",
+    'image': 'assets/images/battlefield.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Cavern",
+    'path': "$_baseUrl/Cavern.mp3",
+    'image': 'assets/images/cavern.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Distant Thunder",
+    'path': "$_baseUrl/1/Distant_Thunder.mp3",
+    'image': 'assets/images/rain.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Drafty Castle",
+    'path': "$_baseUrl/Drafty_Castle.mp3",
+    'image': 'assets/images/demon.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Fireplace",
+    'path': "$_baseUrl/1/Fireplace.mp3",
+    'image': 'assets/images/tavern.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Generic Dungeon",
+    'path': "$_baseUrl/Generic_Dungeon.mp3",
+    'image': 'assets/images/cavern.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Gregorian Voices",
+    'path': "$_baseUrl/1/Gregorian_Voices.mp3",
+    'image': 'assets/images/heaven.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Hearing Voices",
+    'path': "$_baseUrl/Hearing_Voices.mp3",
+    'image': 'assets/images/market.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Heavy Rain",
+    'path': "$_baseUrl/Heavy_Rain.mp3",
+    'image': 'assets/images/rain.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Into Battle",
+    'path': "$_baseUrl/Into_Battle.mp3",
+    'image': 'assets/images/battlefield.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Morning Birds",
+    'path': "$_baseUrl/Morning_Birds.mp3",
+    'image': 'assets/images/sea.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Night Forest",
+    'path': "$_baseUrl/Night_Forest.mp3",
+    'image': 'assets/images/forest.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Our Mountain",
+    'path': "$_baseUrl/Our_Mountain.mp3",
+    'image': 'assets/images/deer.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Pirates",
+    'path': "$_baseUrl/Pirates.mp3",
+    'image': 'assets/images/sea.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Rain",
+    'path': "$_baseUrl/Rain.mp3",
+    'image': 'assets/images/rain.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Sacred Valley",
+    'path': "$_baseUrl/Sacred_Valley.mp3",
+    'image': 'assets/images/heaven.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Sea Coast",
+    'path': "$_baseUrl/Sea_Coast.mp3",
+    'image': 'assets/images/sea.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Tavern",
+    'path': "$_baseUrl/1/Tavern.mp3",
+    'image': 'assets/images/tavern.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
+  {
+    'name': "Village",
+    'path': "$_baseUrl/1/Village.mp3",
+    'image': 'assets/images/market.jpg', 
+    'audio_source': 'file', 
+    'folder_name': 'Thư viện Cloud',
+    'is_offline_mode': true,
+    'version_number': 17
+  },
 ];
 
+// Cập nhật mảng version để báo cho App biết có danh sách âm thanh mới cần nạp vào Database
 List versionNumberWithAudioUpdates = const [
-  16
+  16, 
+  17 // <-- Thêm số 17 vào đây
 ];
